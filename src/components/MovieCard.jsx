bb0a1de90274326403e8ad36c6e1310712c2e450
KSRanasinghe <kesharashakthi71@gmail.com>
2026-04-28 22:32:53 +0530

    feat:retreive data from api and set dynamic movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,23 +1,25 @@
 import { IconStarFilled } from "@tabler/icons-react"
 import { Link } from "react-router"
 
-function MovieCard() {
+function MovieCard(props) {
+  const formattedYear = props.year.length > 4 ? props.year.slice(0, 4) : props.year;
+
   return (
     <>
       <div className="w-full h-auto">
         <div className="w-full h-93 border-4 border-white rounded-lg relative group transition duration-450 ease-linear hover:border-green-500 overflow-hidden">
-          <img src="https://m.media-amazon.com/images/M/MV5BNWE5MGI3MDctMmU5Ni00YzI2LWEzMTQtZGIyZDA5MzQzNDBhXkEyXkFqcGc@._V1_SX300.jpg" alt="Guardians of the Galaxy Vol. 2" className="w-full h-auto max-w-full object-fill block" />
+          <img src={props.poster} alt={props.title} className="w-full max-w-full aspect-2/3 object-cover  block" />
 
           <div className="absolute inset-0 w-full h-full flex flex-col items-center pt-8 pb-4 px-4 bg-gray-950/45 backdrop-blur-sm z-2 opacity-0 transition duration-300 ease-linear group-hover:opacity-100">
             <IconStarFilled className="text-green-500" />
-            <p className="font-arimo font-bold text-white text-3xl mt-3.5">7.6 / 10</p>
-            <h2 className="font-arimo font-bold text-white text-2xl mt-3.5 text-center">Action, Adventure, Comedy</h2>
+            <p className="font-arimo font-bold text-white text-3xl mt-3.5">{props.imdb} / 10</p>
+            <h2 className="font-arimo font-bold text-white text-2xl mt-3.5 text-center">{props.genre}</h2>
             <Link className="font-arimo py-2 px-4 rounded-sm bg-green-500 text-white mt-5 transition duration-450 ease-linear translate-y-6 group-hover:translate-y-0 hover:bg-green-600">View More</Link>
           </div>
         </div>
         <div className="p-1.5">
-          <h3 className="text-white font-arimo text-sm font-light whitespace-break-spaces">Guardians of the Galaxy Vol. 2</h3>
-          <p className="text-white/45 font-arimo text-sm font-light">2017</p>
+          <h3 className="text-white font-arimo text-sm font-light whitespace-break-spaces">{props.title}</h3>
+          <p className="text-white/45 font-arimo text-sm font-light">{formattedYear}</p>
         </div>
       </div>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
 import Wrapper from '../components/Wrapper'
 import MovieCard from '../components/MovieCard'
+import { useEffect, useState } from 'react';
 
 function Home() {
+  const [finalMovie, setFinalMovies] = useState([]);
+  const API_KEY = 'ec51a0d2';
+
+  useEffect(() => {
+    const getInitialMovies = async () => {
+      const URL = `https://www.omdbapi.com/?s=%22new%22&y=2025&apikey=${API_KEY}`;
+
+      try {
+        const res = await fetch(URL);
+        const data = await res.json();
+
+        const detailPromise = data.Search.map(async (movie) => {
+          const detailURL = `https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${API_KEY}`;
+          const detailRes = await fetch(detailURL);
+          return await detailRes.json();
+        });
+
+        const finalMovies = await Promise.all(detailPromise);
+        setFinalMovies(finalMovies);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getInitialMovies();
+  }, []);
+
   return (
     <>
       <Wrapper className='bg-gray-900 py-24 min-h-dvh'>
         <div className='w-full max-w-full px-4'>
           <h1 className='text-center font-arimo text-white font-light text-6xl pb-6'>Welcome to <span className='font-poppins font-bold tracking-tight text-transparent bg-linear-to-r from-green-500 to-cyan-400 bg-clip-text'>MovieApp</span></h1>
-          <p className='text-white font-arimo font-thin tracking-wide text-center text-[16px] w-[60%] leading-8 block mx-auto'>Your ultimate gateway to the world of cinema. Explore millions of movies, dive into detailed ratings, and find your next favorite watch—all powered by the TMDb database.</p>
+          <p className='text-white font-arimo font-thin tracking-wide text-center text-[16px] w-[60%] leading-8 block mx-auto'>Your ultimate gateway to the world of cinema. Explore millions of movies, dive into detailed ratings, and find your next favorite watch—all powered by the OMDb database.</p>
         </div>
         <div className='w-full max-w-full grid grid-cols-4 gap-x-20 gap-y-10 px-4 py-12'>
-          <MovieCard/>
+          {finalMovie.map((movie) => (
+            <MovieCard key={movie.imdbID} title={movie.Title} year={movie.Year} genre={movie.Genre} poster={movie.Poster} imdb={movie.imdbRating} />
+          ))}
         </div>
       </Wrapper>
     </>
