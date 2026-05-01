b77158d5cad7cd225ae3ab7ed5b9f97ddade8ca1
KSRanasinghe <kesharashakthi71@gmail.com>
2026-05-01 18:06:46 +0530

    feat: search bar added

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+import { useId } from "react"
+
+function SearchBar() {
+  const movieInputId = useId();
+
+  return (
+    <>
+      <label htmlFor={movieInputId} className="font-arimo text-xl font-semibold text-white/65 block">Search Movie:</label>
+      <div className="pt-2 flex items-center gap-8">
+        <input id={movieInputId} type="text" className="grow-2 bg-white/20 rounded-sm py-2 px-3 focus:outline-0 font-arimo text-white/70" />
+        <button type="submit" className="font-arimo py-2 px-4 rounded-sm bg-green-500 text-white transition duration-450 ease-linear hover:bg-green-600">Search</button>
+      </div>
+    </>
+  )
+}
+
+export default SearchBar
--- src/pages/BrowseMovies.jsx
+++ src/pages/BrowseMovies.jsx
@@ -1,16 +1,21 @@
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import SearchBar from "../components/SearchBar"
 import Wrapper from "../components/Wrapper"
 
 function BrowseMovies() {
   return (
     <>
       <Navbar />
-      <Wrapper className='bg-gray-900 pt-24 min-h-dvh'>
+      <Wrapper className='bg-gray-900/95 py-15'>
         <div className='w-full max-w-full px-4'>
-          
+          <h1 className='text-center font-arimo text-white font-light text-5xl pb-6'>Search What <span className='font-poppins font-bold tracking-tight text-transparent bg-linear-to-r from-green-500 to-cyan-400 bg-clip-text'>You Love</span></h1>
+          <div className="max-w-2xl mx-auto">
+            <SearchBar />
+          </div>
         </div>
       </Wrapper>
+      <Wrapper className="bg-gray-900 py-20 min-h-dvh"></Wrapper>
       <Footer />
     </>
   )
